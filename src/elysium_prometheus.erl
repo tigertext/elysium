40b1078d0bbc007240ccfe4b302b3f28cf3deddc
Leo Wang <lwang@tigertext.com>
2019-04-24 15:43:31 +0800

    Update default bucket

--- src/elysium_prometheus.erl
+++ src/elysium_prometheus.erl
@@ -16,7 +16,7 @@ is_prometheus_started() ->
 
 do_setup() ->
     Get_Connection_Duration_Buckets =
-        case application:get_env(elysium, prometheus_default_cassandra_get_connection_duration_buckets, [25, 40, 60, 100, 125, 150, 200, 300, 450, 600, 800, 1000]) of
+        case application:get_env(elysium, prometheus_default_cassandra_get_connection_duration_buckets, [2, 4, 6, 10, 12, 15, 20, 30, 45, 60, 80, 100]) of
             {ok, V1} -> V1;
             V2 -> V2
         end,
@@ -27,7 +27,7 @@ do_setup() ->
         {buckets, Get_Connection_Duration_Buckets},
         {help, "The time to get cassandra connection from connection pool in milliseconds."}]),
     Exec_Cmd_Duration_Buckets =
-        case application:get_env(elysium, prometheus_default_cassandra_exec_cmd_duration_buckets, [25, 40, 60, 100, 125, 150, 200, 300, 450, 600, 800, 1000]) of
+        case application:get_env(elysium, prometheus_default_cassandra_exec_cmd_duration_buckets, [2, 4, 6, 10, 12, 15, 20, 30, 45, 60, 80, 100]) of
             {ok, V3} -> V3;
             V4 -> V4
         end,
