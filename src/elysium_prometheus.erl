59e38e2796e5f70bc226919875bf5db9a62fc28b
Leo Wang <lwang@tigertext.com>
2019-04-24 18:05:40 +0800

    Change from milliseconds to microseconds

--- src/elysium_prometheus.erl
+++ src/elysium_prometheus.erl
@@ -16,27 +16,27 @@ is_prometheus_started() ->
 
 do_setup() ->
     Get_Connection_Duration_Buckets =
-        case application:get_env(elysium, prometheus_default_cassandra_get_connection_duration_buckets, [2, 4, 6, 10, 12, 15, 20, 30, 45, 60, 80, 100]) of
+        case application:get_env(elysium, prometheus_default_cassandra_get_connection_duration_buckets, [125, 250, 500, 750, 1000, 1250, 1500, 2000, 3000, 5000, 7500, 10000]) of
             {ok, V1} -> V1;
             V2 -> V2
         end,
     prometheus_histogram:declare([
-        {name, cassandra_get_connection_duration_milliseconds},
+        {name, cassandra_get_connection_duration_microseconds},
         {registry, ?DEFAULT_REGISTRY},
         {labels, [originating_server, destination_keyspace, destination_table, command_type]},
         {buckets, Get_Connection_Duration_Buckets},
-        {help, "The time to get cassandra connection from connection pool in milliseconds."}]),
+        {help, "The time to get cassandra connection from connection pool in microseconds."}]),
     Exec_Cmd_Duration_Buckets =
-        case application:get_env(elysium, prometheus_default_cassandra_exec_cmd_duration_buckets, [2, 4, 6, 10, 12, 15, 20, 30, 45, 60, 80, 100]) of
+        case application:get_env(elysium, prometheus_default_cassandra_exec_cmd_duration_buckets, [125, 250, 500, 750, 1000, 1250, 1500, 2000, 3000, 5000, 7500, 10000]) of
             {ok, V3} -> V3;
             V4 -> V4
         end,
     prometheus_histogram:declare([
-        {name, cassandra_exec_cmd_duration_milliseconds},
+        {name, cassandra_exec_cmd_duration_microseconds},
         {registry, ?DEFAULT_REGISTRY},
         {labels, [originating_server, destination_keyspace, destination_table, command_type]},
         {buckets, Exec_Cmd_Duration_Buckets},
-        {help, "The time to execute cassandra commands against DB in milliseconds."}]).
+        {help, "The time to execute cassandra commands against DB in microseconds."}]).
 
 report_metrics(_Metric_Name, undefined, _Duration) ->
     ok;
